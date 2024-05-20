fb00587a69d4b9020d548a1ba7d6da68df1b5f4f
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-05-20 15:49:59 +0200

    Update seed.ts

--- lib/seed.ts
+++ lib/seed.ts
@@ -22,11 +22,36 @@ const seedUsers = async (client: VercelClient) => {
   }
 }
 
+const seedUrls = async (client: VercelClient) => {
+    try {
+        await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+        // Create the "urls" table if it doesn't exist
+        const createTable = await client.sql`
+        CREATE TABLE IF NOT EXISTS urls (
+            user_id UUID NOT NULL REFERENCES users(id),
+            original_url TEXT NOT NULL,
+            short_url TEXT NOT NULL PRIMARY KEY,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            expires_at TIMESTAMP
+            views INT DEFAULT 0
+        );
+        `;
+        console.log(`Created "urls" table`);
+        return { createTable };
+    } catch (error) {
+        console.error('Error seeding urls:', error);
+        throw error;
+    }
+}
+
+            
+
 const main = async () => {
   const client = createClient();
   await client.connect()
 
   await seedUsers(client);
+    await seedUrls(client);
 
   await client.end();
 }
