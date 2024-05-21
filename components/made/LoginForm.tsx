1248c5f936bdf08bdd92e6bc8df4e8a502e03bc1
wlanhage <wlanhage@gmail.com>
2024-05-21 13:01:10 +0200

    added styling on homepage and login

--- components/made/LoginForm.tsx
+++ components/made/LoginForm.tsx
@@ -3,6 +3,9 @@
 import { ChangeEvent, FC, FormEvent, FormHTMLAttributes, useState } from 'react';
 import { signIn } from "next-auth/react";
 import { useRouter } from 'next/navigation';
+import { Button } from "../ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage  } from '../ui/form';
+import { Input } from '../ui/input';
 
 type LoginFormProps = FormHTMLAttributes<HTMLFormElement> & {};
 
@@ -27,11 +30,22 @@ const LoginForm: FC<LoginFormProps> = ({ ...props }) => {
         }
     }
     
-  return <form {...props} className='text-black pt-16' onSubmit={handleSubmit}>
-    <input placeholder='Email' value={form.email} onChange={(ev) => handleChange("email", ev)} type="text" name='email' autoComplete='email' autoFocus />
-    <input placeholder='Password' value={form.password} onChange={(ev) => handleChange("password", ev)} type="password" name='password' autoComplete='current-password' />
-    <button type='submit'>Login</button>
-  </form>;
+  return (
+    <div className="text-center mb-8">
+    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+      Minify
+    </h2>
+    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+      Login
+    </h4>
+
+    <form {...props} className='text-black pt-16' onSubmit={handleSubmit}>
+      <Input placeholder='Email' value={form.email} onChange={(ev) => handleChange("email", ev)} type="text" name='email' autoComplete='email' autoFocus className='mb-5'/>
+      <Input placeholder='Password' value={form.password} onChange={(ev) => handleChange("password", ev)} type="password" name='password' autoComplete='current-password' className='mb-8'/>
+      <Button type='submit' style={{width: '200px'}}>Login</Button>
+    </form>
+  </div>
+  )
 };
 
 export default LoginForm;
