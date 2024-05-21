c5b979638d83bfbc92d4ad74de85c2a29b53d7e7
William Davidson <wj.davidson@outlook.com>
2024-05-21 15:58:51 +0200

    fixed auth session

---
+++ app/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,50 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt"
+import { sql } from "@vercel/postgres";
+
+const authOptions: NextAuthOptions = {
+    session: {
+        strategy: "jwt"
+    },
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+              email: {},
+              password: {}
+            },
+            async authorize(credentials, req) {                
+                if (!credentials) return null;
+                
+                const res = await sql`
+                    SELECT * FROM users WHERE email=${credentials.email}
+                `
+                const user = res.rows[0];
+
+                const passOk = await bcrypt.compare(credentials.password || "", user.password);
+                                
+                if (passOk) {
+                    return {
+                        id: user.id,
+                        name: user.name,
+                        email: user.email
+                    }
+                }
+                
+                return null;
+            }
+          })
+    ],
+    callbacks: {
+        async session(sessionParams) {
+            const { session, token } = sessionParams
+
+            session.user = { ...session.user, id: token.sub || ""}
+
+            return session;
+        }
+    }
+}
+
+export default authOptions
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,11 +2,12 @@ import { FC } from 'react';
 import { getServerSession } from "next-auth";
 import LoginForm from '@/components/made/LoginForm';
 import { redirect } from 'next/navigation';
+import authOptions from '../api/auth/[...nextauth]/auth';
 
 type LoginProps = {};
 
 const Login: FC<LoginProps> = async ({}) => {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   if (session) redirect("/")
   
