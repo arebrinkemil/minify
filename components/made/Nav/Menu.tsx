ced844b2a7a3487945d65bb3db0751e2134db864
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-05-27 13:49:58 +0200

    added switch for darkmode

--- components/made/Nav/Menu.tsx
+++ components/made/Nav/Menu.tsx
@@ -1,65 +1,72 @@
-"use client"
+'use client'
 
-import { FC } from 'react';
-import { Session } from 'next-auth';
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
-import Link from 'next/link';
-import Image from 'next/image';
-import { signOut } from 'next-auth/react';
-import { Button } from '@/components/ui/button';
-import { toast } from 'sonner';
+import { FC } from 'react'
+import { Session } from 'next-auth'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+import Link from 'next/link'
+import Image from 'next/image'
+import { signOut } from 'next-auth/react'
+import { Button } from '@/components/ui/button'
+import { toast } from 'sonner'
+import { ModeToggle } from './ToggleMode'
+import { Link as LinkLogo } from 'lucide-react'
 
 type MenuProps = {
-    session: Session | null;
-};
+  session: Session | null
+}
 
-const Menu: FC<MenuProps> = ({ session }) => {    
+const Menu: FC<MenuProps> = ({ session }) => {
   return (
-    <NavigationMenu className='flex justify-between items-center w-screen max-w-screen h-fit bg-zinc-100 py-1 px-4 md:px-8 border-b border-s-zinc-200 fixed z-10 inset-0'>
-        <NavigationMenuList>
+    <NavigationMenu className='max-w-screen fixed inset-0 z-10 flex h-[79px] w-screen items-center justify-between border-b border-s-zinc-200 bg-background px-4 py-1 md:px-8'>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href='/'>
+            <LinkLogo className='' size={45} />
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuList>
+        {!!session ? (
+          <>
             <NavigationMenuItem>
-                <Link href="/">
-                    <Image src="/minify.png" alt='Minify' width={70} height={70}></Image>
-                </Link>
+              <Button asChild>
+                <Link href={'/dashboard'}>Dashboard</Link>
+              </Button>
             </NavigationMenuItem>
-        </NavigationMenuList>
-        <NavigationMenuList>
-            {!!session ? (
-                <>
-                    <NavigationMenuItem>
-                        <Button asChild>
-                            <Link href={"/dashboard"}>
-                                Dashboard
-                            </Link>
-                        </Button>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem asChild>
-                        <Button onClick={() => signOut()}>
-                            Sign out
-                        </Button>
-                    </NavigationMenuItem>
-                </>
-            ) : (
-                <>
-                    <NavigationMenuItem>
-                        <Button asChild>
-                            <Link href={"/login"}>
-                                Login
-                            </Link>
-                        </Button>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Button asChild>
-                            <Link href={"/register"}>
-                                Register
-                            </Link>
-                        </Button>
-                    </NavigationMenuItem>
-                </>
-            )}
-        </NavigationMenuList>
+            <NavigationMenuItem asChild>
+              <Button onClick={() => signOut()}>Sign out</Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <ModeToggle />
+            </NavigationMenuItem>
+          </>
+        ) : (
+          <>
+            <NavigationMenuItem>
+              <Button asChild>
+                <Link href={'/login'}>Login</Link>
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Button asChild>
+                <Link href={'/register'}>Register</Link>
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <ModeToggle />
+            </NavigationMenuItem>
+          </>
+        )}
+      </NavigationMenuList>
     </NavigationMenu>
   )
-};
+}
 
-export default Menu;
+export default Menu
